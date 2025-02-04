b12880c7bff8fac07c3a7b5810e5ab578df90847
Manzana1 <61259393+Manzana1@users.noreply.github.com>
2025-02-03 21:16:27 -0500

    adding hover with CardWrapper

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,5 +1,6 @@
 import PropTypes from 'prop-types'
 import styled from 'styled-components'
+import colors from '../../colors'
 
 const CardLabel = styled.span`
   color: #5843e4;
@@ -11,21 +12,27 @@ const CardImage = styled.img`
   width: 80px;
   border-radius: 50%;
 `
+const CardWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 15px;
+  background-color: ${colors.backgroundLight};
+  border-radius: 30px;
+  width: 350px;
+  transition: 200ms;
+  &:hover {
+    cursor: pointer;
+    box-shadow: 2px 2px 10px #e2e3e9;
+  }
+`
 
 function Card({ label, title, picture }) {
   return (
-    <div
-      style={{
-        display: 'flex',
-        flexDirection: 'column',
-        padding: 15,
-        alignItems: 'center',
-      }}
-    >
+    <CardWrapper>
       <CardLabel>{label}</CardLabel>
       <CardImage src={picture} alt="freelance" />
       <span>{title}</span>
-    </div>
+    </CardWrapper>
   )
 }
 
